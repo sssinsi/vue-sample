4253ceacdf01f5c61a5d939d82d8cadbeb951828
sssinsi <tet643773@gmail.com>
2017-04-28 14:41:16 +0900

    first commit

---
+++ tutorial/src/components/Sample.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>Sample!!!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sample',
+  data () {
+    return {
+      msg: 'Hi!!!!!!!'
+    }
+  }
+}
+</script>
+
+
+<style></style>
